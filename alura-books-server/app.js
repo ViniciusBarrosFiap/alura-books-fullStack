65d63b4475bd9c8ec1061e438da3d90f31017b8d
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-09-11 15:42:49 -0300

    fazendo a rotas no app.js, terminar

--- alura-books-server/app.js
+++ alura-books-server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const rotaLivro = require("./rotas/livro")
+const rotaFavoritos = require("./rotas/favorito")
 const app = express();
 app.use(express.json())
 
--- alura-books-server/servicos/livro.js
+++ alura-books-server/servicos/livro.js
@@ -4,7 +4,7 @@ function getTodosLivros(){
     return JSON.parse(fs.readFileSync("livros.json"))
 }
 function getLivroPorId(id) {
-    const livros = JSON.parse(fs.readFileSync("livros.json"))
+const livros = JSON.parse(fs.readFileSync("livros.json"))
  
     const livroFiltrado = livros.filter( livro => livro.id === id )[0]
     return livroFiltrado
